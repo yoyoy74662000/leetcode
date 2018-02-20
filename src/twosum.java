17934db5d5e9cfd0da0ed90013c5307459816035
yoyoy74662000 <yoyoy74662000@github.com>
2018-02-19 22:15:54 -0600

    twosum

--- src/twosum.java
+++ src/twosum.java
@@ -1,3 +1,23 @@
 public class twosum {
+    //step 1
+    //base case
+        if(nums ==null || nums.length<2){
+        return new int[]{-1,-1};
+    }
+    int res[] = new int[]{-1,-1};
+    // step 3 hashmap
+    HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i =0; i<nums.length; i++){
+        if(map.containsKey(target-nums[i])){
+            res[0]=map.get(target-nums[i]);
+            res[1]=i;
+            break;
+        }
+        //step 2
+        map.put(nums[i],i);
+    }
+        return res;
+
+
 
 }
