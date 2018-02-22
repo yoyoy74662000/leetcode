1e4f08e424807722c7df81df725002c636510896
yoyoy74662000 <yoyoy74662000@github.com>
2018-02-22 13:54:47 -0600

    add new solution

---
+++ src/BalancedBinaryTree.java
@@ -0,0 +1,36 @@
+import javax.swing.tree.TreeNode;
+
+/**
+ * Given a binary tree, determine if it is height-balanced.
+ * For this problem, a height-balanced binary tree is defined as:
+ * a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+ *
+ * Given the following tree [3,9,20,null,null,15,7]:
+ *
+        3
+       / \
+      9  20
+    /  \
+   15   7
+ *
+ *
+ */
+
+
+public class BalancedBinaryTree {
+    public boolean isBalanced(TreeNode root) {
+        if( root == null ) return true;
+        return helper(root) != -1;
+    }
+
+    public int helper( TreeNode root){
+        if( root == null) return 0;
+        int left = helper(root.left);
+        int right = helper(root.right);
+        if(left == -1 || right == -1 || Math.abs(left - right) > 1){
+            return -1;
+        } else{
+            return Math.max(left,right)+1;
+        }
+    }
+}
