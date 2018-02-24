f21be9f95622090cc92873fa83d2231c5f4cff6a
yoyoy74662000 <yoyoy74662000@github.com>
2018-02-24 16:08:41 -0600

    add new solution

--- src/IntersectionofTwoLinkedLists.java
+++ src/IntersectionofTwoLinkedLists.java
@@ -10,9 +10,24 @@
  *                     ↗
  * B:      b1 → b2 → b3
  *
- *
+ * A: a1 → a2 → c1 → c2 → c3 → b1 → b2 → b3 → c1 → c2 → c3
+ * B: b1 → b2 → b3 → c1 → c2 → c3 → a1 → a2 → c1 → c2 → c3
  */
 
+// time : O(m+n)
+// space : O(1)
 
 public class IntersectionofTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) return null;
+        ListNode a = headA;
+        ListNode b = headB;
+        while( a != b ){
+            // 如果 a != null 就到 a.next == null 就到 headB
+            a = a == null ? headB : a.next;
+            // 如果 b != null 就到 b.next == null 就到 headA
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
 }
