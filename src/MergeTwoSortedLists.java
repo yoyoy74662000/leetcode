4cc3ecc7c3a1b10aab4a147a69ff1dd33a4ed508
yoyoy74662000 <yoyoy74662000@github.com>
2018-02-24 22:03:08 -0600

    add new solution

--- src/MergeTwoSortedLists.java
+++ src/MergeTwoSortedLists.java
@@ -13,7 +13,7 @@
 // 遞歸
 public class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null && l2 == null);
+        if(l1 == null && l2 == null) return null;;
         if(l1 == null) return l2;
         if(l2 == null)  return l1;
 
@@ -33,7 +33,7 @@ public class MergeTwoSortedLists {
 // dummy 方法
 public class MergeTwoSortedLists2 {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null && l2 ==null) return null;
+        if(l1 == null && l2 == null) return null;
         ListNode dummy = new ListNode(0);
         ListNode cur = dummy;
         while(l1 != null && l2 != null){
