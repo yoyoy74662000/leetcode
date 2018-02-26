53bcf946d865d638077693fea70c49e5c46150e5
yoyoy74662000 <yoyoy74662000@github.com>
2018-02-26 03:09:37 -0600

    add new solution

--- src/reverselinkedlist.java
+++ src/reverselinkedlist.java
@@ -1,2 +1,21 @@
+/**
+ * Reverse a singly linked list.
+ *
+ *
+ *
+ *
+ */
+
+// time : O(n)
+// space : O(n)
+
 public class reverselinkedlist {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode root = reverseList(head.next);
+
+        head.next.next = head;
+        head.next = null;
+        return root;
+    }
 }
