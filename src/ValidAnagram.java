1977d312d7eea065d597538be2ec06c677dd9bf3
yoyoy74662000 <yoyoy74662000@github.com>
2018-02-26 21:55:36 -0600

    add new solution

--- src/ValidAnagram.java
+++ src/ValidAnagram.java
@@ -27,14 +27,18 @@ public class ValidAnagram {
 
 public class ValidAnagram2 {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
+        if(s.length() != t.length()) {
+            return false;
+        }
         int[] count = new int[26];
-        for(int i =0; i<s.length()-1;i++){
-            count[s.charAt(i)-'a']++;
-            count[t.charAt(i)-'a']--;
+        for(int i =0; i < s.length() - 1;i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
         }
         for(int num : count){
-            if(num != 0) return false;
+            if(num != 0) {
+                return false;
+            }
         }
         return true;
     }
