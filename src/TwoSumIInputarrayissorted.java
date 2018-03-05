1b9be77bd33b616aea675435ff248f2c1501ee66
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-04 18:51:22 -0600

    add new solution

---
+++ src/TwoSumIInputarrayissorted.java
@@ -0,0 +1,55 @@
+/**
+ * Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+ * The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+ * Please note that your returned answers (both index1 and index2) are not zero-based.
+ * You may assume that each input would have exactly one solution and you may not use the same element twice.
+ * Input: numbers={2, 7, 11, 15}, target=9
+ * Output: index1=1, index2=2
+ *
+ */
+
+
+
+// time : O(n) / O(logn)
+// space : O(1) / O(1)
+
+public class TwoSumIInputarrayissorted {
+    public int[] twoSum(int[] numbers, int target) {
+        if(numbers ==null || numbers.length<2){
+            return new int[]{-1,-1};
+        }
+        int res[] = new int[]{-1,-1};
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i =0; i<numbers.length; i++){
+            if(map.containsKey(target-numbers[i])){
+                res[0]=map.get(target-numbers[i]) + 1;
+                res[1]=i + 1;
+                break;
+            }
+            map.put(numbers[i],i);
+        }
+        return res;
+    }
+
+}
+
+public class TwoSumIInputarrayissorted2 {
+    public int[] twoSum(int[] numbers, int target) {
+        if(numbers ==null || numbers.length<2){
+            return new int[]{-1,-1};
+        }
+        int left = 0;
+        int right = numbers.length - 1;
+        while(left < right){
+            int sum = numbers[left] + numbers[right];
+            if (sum == target){
+                return new int[]{left + 1, right + 1};
+            }else if(sum > target){
+                right--;
+            }else{
+                left++;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
