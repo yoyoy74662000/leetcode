cab610579b80a0b18ed0e547d98bb225115cdc75
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-05 15:32:54 -0600

    add new solution

---
+++ src/ContainsDuplicate.java
@@ -0,0 +1,16 @@
+/**
+ * Given an array of integers, find if the array contains any duplicates.
+ * Your function should return true if any value appears at least twice in the array,
+ * and it should return false if every element is distinct.
+ *
+ */
+
+
+// time : O(n)
+// space : O(n)
+
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+
+    }
+}
--- src/MajorityElement.java
+++ src/MajorityElement.java
@@ -13,18 +13,16 @@ import java.util.HashMap;
 public class MajorityElement {
     public int majorityElement(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        int res = 0;
         for (int num : nums){
             if(! map.containsKey(num)){
                 map.put(num, 1);
             }else{
                 map.put(num, map.get(num) + 1);
             }
-            if(map.get(num) > nums.length / 2){
-                res = num;
-                break;
+            if(map.get(num) >=  2){
+                return true;
             }
-        }return res;
+        }return false;
     }
 
 }
