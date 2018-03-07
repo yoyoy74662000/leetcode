c36cafb6ff6fd7ae89a4a0244b37525f0b8343b0
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-07 15:09:13 -0600

    add new solution

---
+++ src/BinaryTreePaths.java
@@ -0,0 +1,20 @@
+/**
+ * Given a binary tree, return all root-to-leaf paths.
+ * For example, given the following binary tree:
+ *    1
+    /   \
+   2     3
+   \
+    5
+ *
+ *
+ */
+
+// time : O(n^2)
+// space : O(1)
+
+public class BinaryTreePaths {
+    public List<String> binaryTreePaths(TreeNode root) {
+
+    }
+}
