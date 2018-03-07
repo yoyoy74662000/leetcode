db564241ae4e63c24a06fae5fde1b622ebc021f6
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-07 15:02:33 -0600

    add new solution

--- src/ImplementstrStr.java
+++ src/ImplementstrStr.java
@@ -6,12 +6,11 @@
 // time : O(n^2)
 // space : O(1)
 
-
 public class ImplementstrStr {
     public int strStr(String haystack, String needle) {
         if (needle == null || needle.length() == 0) return 0;
-        for(int i =0; i < haystack.length() - needle.length(); i++){
-            if (haystack.substring(i, needle.length()).equals(needle))
+        for(int i =0; i <= haystack.length() - needle.length(); i++){
+            if (haystack.substring(i, i + needle.length()).equals(needle))
                 return i;
         }
         return -1;
