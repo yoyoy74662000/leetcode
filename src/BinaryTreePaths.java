f9f78134fe006ca552b1ef936e01ef2153693443
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-07 15:39:37 -0600

    add new solution

--- src/BinaryTreePaths.java
+++ src/BinaryTreePaths.java
@@ -1,3 +1,6 @@
+import javax.swing.tree.TreeNode;
+import java.util.List;
+
 /**
  * Given a binary tree, return all root-to-leaf paths.
  * For example, given the following binary tree:
@@ -10,11 +13,24 @@
  *
  */
 
-// time : O(n^2)
-// space : O(1)
+// time : O(n)
+// space : O(n)
 
 public class BinaryTreePaths {
     public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<String>();
+        if (root == null) return res;
+        helper(res, root, "");
+        return res;
+    }
+
+    public static void helper(List<String> res, TreeNode root, String path){
+        if(root.left == null && root.right == null)
+            res.add(path + root.val);
+        if (root.left != null)
+            helper(res, root.left, path + root.val + "->");
+        if (root.right != null)
+            helper(res, root.right, path + root.val + "->");
 
     }
 }
