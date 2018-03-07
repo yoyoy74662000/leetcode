d886d454d4d1550a5ad0f5107d212c349d1b0003
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-07 16:51:02 -0600

    add new solution

---
+++ src/AddTwoNumbers.java
@@ -0,0 +1,39 @@
+/**
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order and each of their nodes contain a single digit.
+ * Add the two numbers and return it as a linked list.
+ *
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ *
+ */
+
+// time : O(n)
+// space : O(n)
+
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        ListNode p1 = l1, p2 = l2;
+        int sum = 0;
+        while (p1 != null || p2 != null){
+            if (p1 != null){
+                sum += p1.val;
+                p1 = p1.next;
+            }
+            if (p2 != null) {
+                sum += p2.val;
+                p2 = p2.next;
+            }
+            cur.next = new ListNode(sum % 10);
+            sum = sum / 10;
+            cur = cur.next;
+        }
+        if (sum == 1){
+            cur.next = new ListNode(1);
+        }
+        return dummy.next;
+    }
+}
