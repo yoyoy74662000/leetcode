af8c35e2642f7648c506114c6d64f092cefc29cc
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-12 17:20:33 -0500

    add new solution

---
+++ src/CombinationSum.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a set of candidate numbers (C) (without duplicates) and a target number (T),
+ * find all unique combinations in C where the candidate numbers sums to T.
+ * The same repeated number may be chosen from C unlimited number of times.
+ * For example, given candidate set [2, 3, 6, 7] and target 7,
+ * A solution set is:
+ * [
+    [7],
+    [2, 2, 3]
+   ]
+ *
+ *
+ */
+
+// time : O(n^2)
+// space : O(n)
+
+public class CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (candidates == null || candidates.length == 0) return res;
+        helper(res, new ArrayList<>(), candidates, target, 0);
+        return res;
+    }
+
+    public void helper(List<List<Integer>> res, List<Integer> list, int[] candidates, int target, int start){
+        if (target < 0) return;
+        if (target == 0){
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i = start; i < candidates.length; i++){
+            list.add(candidates[i]);
+            helper(res, list, candidates, target - candidates[i], start);
+            list.remove(list.size() -1 );
+        }
+    }
+}
