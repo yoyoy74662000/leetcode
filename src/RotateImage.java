3a92ba5be0b29813bd52136dd4d362db665e588e
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-12 23:42:36 -0500

    add new solution

---
+++ src/RotateImage.java
@@ -0,0 +1,42 @@
+/**
+ * You are given an n x n 2D matrix representing an image.
+ * Rotate the image by 90 degrees (clockwise).
+
+ Given input matrix =
+ [
+ [1,2,3],
+ [4,5,6],
+ [7,8,9]
+ ],
+
+ rotate the input matrix in-place such that it becomes:
+ [
+ [7,4,1],
+ [8,5,2],
+ [9,6,3]
+ ]
+
+ */
+
+// time : O(n*m)
+// space : O(n)
+
+public class RotateImage {
+    public void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = i; j < matrix[0].length; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix.length / 2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length- 1 - j];
+                matrix[i][matrix.length- 1 - j] = temp;
+            }
+        }
+    }
+}
