08e5c28edfde04ddb1fabdf5734189fc5011b301
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-13 17:23:33 -0500

    add new solution

---
+++ src/MergeIntervals.java
@@ -0,0 +1,17 @@
+/**
+ * Given a collection of intervals, merge all overlapping intervals.
+ * For example,
+ * Given [1,3],[2,6],[8,10],[15,18],
+ * return [1,6],[8,10],[15,18].
+ *
+ *
+ */
+
+// time : O(n)
+// space : O(1)
+
+public class MergeIntervals {
+    public List<Interval> merge(List<Interval> intervals) {
+
+    }
+}
