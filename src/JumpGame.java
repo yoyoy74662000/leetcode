63a5dfb7f9331df1323287debb967e7f32ca0a84
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-13 17:22:09 -0500

    add new solution

--- src/JumpGame.java
+++ src/JumpGame.java
@@ -12,10 +12,17 @@
  *
  */
 
-
+// time : O(n)
+// space : O(1)
 
 public class JumpGame {
     public boolean canJump(int[] nums) {
+        int max =0;
+        for (int i = 0; i < nums.length; i++){
+            if (i > max) return false;
+            max = Math.max(nums[i] + i, max);
+        }
+        return true;
 
     }
 }
