2e69bcc525973e24cf2bafa68b86a0ce22553918
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-15 01:44:10 -0500

    add new solution

--- src/Subsets.java
+++ src/Subsets.java
@@ -3,6 +3,20 @@ import java.util.ArrayList;
 /**
  * Given a set of distinct integers, nums, return all possible subsets (the power set).
  * Note: The solution set must not contain duplicate subsets.
+ * If nums = [1,2,3], a solution is:
+ * [
+    [3],
+    [1],
+    [2],
+    [1,2,3],
+    [1,3],
+    [2,3],
+    [1,2],
+    []
+   ]
+ *
+ *
+ *
  */
 
 
