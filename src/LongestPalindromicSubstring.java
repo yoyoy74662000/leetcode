cda195c9fefe57e83419e2355b98e5ae01a8bfbd
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-18 18:03:53 -0500

    add new solution

--- src/LongestPalindromicSubstring.java
+++ src/LongestPalindromicSubstring.java
@@ -9,9 +9,28 @@
  * Output: "bb"
  */
 
+// time : O(n^2)
+// space : O(1)
 
 public class LongestPalindromicSubstring {
     public String longestPalindrome(String s) {
+        if (s ==null || s.length() == 0) return null;
+        for (int i = 0; i < s.length(); i++){
+            helper(s, i, i);
+            helper(s, i, i + 1);
+        }
+        return res;
+    }
 
+    public static helper(String s, int left, int right){
+        String res = "";
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+        String cur = s.substring(left + 1, right);
+        if (cur.length() > res.length()){
+            res = cur;
+        }
     }
 }
