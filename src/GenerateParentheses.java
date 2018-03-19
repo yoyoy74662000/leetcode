be5fc5e95cc2a8d89f09e58331c7f720939dbc36
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-19 15:10:01 -0500

    add new solution

--- src/GenerateParentheses.java
+++ src/GenerateParentheses.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
  * For example, given n = 3, a solution set is:
@@ -12,6 +15,26 @@
  */
 public class GenerateParentheses {
     public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        if(n == 0) return res;
+        helper(res, "", n, n);
+        return res;
+    }
 
+    public static void helper(List<String> res, String s, int left, int right){
+        if (left > right){
+            return;
+        }
+
+        if (left == 0 && right == 0){
+            res.add(s);
+        }
+
+        if (left > 0){
+            helper(res, s + '(', left -1, right);
+        }
+        if (right > 0){
+            helper(res, s + ')', left, right -1);
+        }
     }
 }
