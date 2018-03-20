bdb34d043c57b22f233774e0aef99bcd702f9117
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-19 19:21:10 -0500

    add new solution

---
+++ src/GroupAnagrams.java
@@ -0,0 +1,21 @@
+/**
+ * Given an array of strings, group anagrams together.
+ * For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * Return:
+ *
+ * [
+ * ["ate", "eat","tea"],
+ * ["nat","tan"],
+ * ["bat"]
+ * ]
+ *
+ */
+
+// time : O(2^n)
+// space : O(n)
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+    }
+}
