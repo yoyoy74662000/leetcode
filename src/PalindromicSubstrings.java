a78e16166ee1d859004bea89f4546edf1fadddfe
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-19 23:55:01 -0500

    add new solution

--- src/PalindromicSubstrings.java
+++ src/PalindromicSubstrings.java
@@ -1,3 +1,17 @@
+/**
+ * Given a string, your task is to count how many palindromic substrings in this string.
+ * The substrings with different start indexes or end indexes are counted as different substrings
+ * even they consist of same characters.
+ *
+ * Input: "abc"
+ * Output: 3
+ * Explanation: Three palindromic strings: "a", "b", "c".
+ *
+ * Input: "aaa"
+ * Output: 6
+ * Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ *
+ */
 public class PalindromicSubstrings {
     public int countSubstrings(String s) {
 
