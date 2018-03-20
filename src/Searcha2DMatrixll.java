ee8a386a3dd8d4b35bce7896f1b5f439330f3908
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-20 15:05:41 -0500

    add new solution

--- src/Searcha2DMatrixll.java
+++ src/Searcha2DMatrixll.java
@@ -22,7 +22,18 @@
 public class Searcha2DMatrixll {
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0) return false;
-
+        int row = matrix.length , col = matrix[0].length - 1;
+        while (col >= 0 && row < matrix.length - 1){
+            if (target == matrix[row][col]){
+                return true;
+            }else if(target < matrix[row][col]){
+                col--;
+            }
+            else {
+                row++;
+            }
+        }
+        return false;
     }
 
 }
