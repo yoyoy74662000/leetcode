51d33b64213df5a7b798d1d93a046444323af624
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-19 20:23:14 -0500

    add new solution

--- src/GroupAnagrams.java
+++ src/GroupAnagrams.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Given an array of strings, group anagrams together.
  * For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"],
@@ -16,6 +21,24 @@
 
 public class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res = new ArrayList<>();
+        if (strs == null || strs.length == 0) return res;
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String str : strs ){
+            char[] ch = str.toCharArray();
+            Arrays.sort(ch);
+            String s = new String(ch);
+            if (map.containsKey(s)){
+                List<String> list = res.get(map.get(s));
+                list.add(str);
+            }else{
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(s,res.size());
+                res.add(list);
+            }
+        }
+        return res;
 
     }
 }
