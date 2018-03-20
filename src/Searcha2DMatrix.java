2d4f349c5545a35ddbd45deddce24dcc4ed35067
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-20 14:42:41 -0500

    add new solution

--- src/Searcha2DMatrix.java
+++ src/Searcha2DMatrix.java
@@ -1,9 +1,14 @@
 /**
- * Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
-
- Integers in each row are sorted from left to right.
- The first integer of each row is greater than the last integer of the previous row.
- *
+ * Write an efficient algorithm that searches for a value in an m x n matrix.
+ * This matrix has the following properties:
+ * Integers in each row are sorted from left to right.
+ * The first integer of each row is greater than the last integer of the previous row.
+ * [
+ * [1,   3,  5,  7],
+ * [10, 11, 16, 20],
+ * [23, 30, 34, 50]
+ * ]
+ * Given target = 3, return true.
  *
  */
 
@@ -15,9 +20,10 @@ public class Searcha2DMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0) return false;
         int row = matrix.length , col = matrix[0].length;
-        int start = 0, end = row * col -1;
+        int start = 0; //0
+        int end = row * col -1; //11
         while(start <= end){
-            int mid = (end - start) / 2 + start;
+            int mid = (end - start) / 2 + start; //5
             int value = matrix[mid / col][mid % col];
             if (value == target){
                 return true;
--- src/SearchforaRange.java
+++ src/SearchforaRange.java
@@ -1,6 +1,6 @@
 /**
- * Given an array of integers sorted in ascending order, f
- * ind the starting and ending position of a given target value.
+ * Given an array of integers sorted in ascending order,
+ * find the starting and ending position of a given target value.
  * Your algorithm's runtime complexity must be in the order of O(log n).
  * If the target is not found in the array, return [-1, -1].
  * For example,
