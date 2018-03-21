5da2b4dd03cd8dc02f8c670b999d44ca292d488d
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-21 15:02:27 -0500

    add new solution

--- src/KthLargestElementinanArray.java
+++ src/KthLargestElementinanArray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Find the kth largest element in an unsorted array.
  * Note that it is the kth largest element in the sorted order, not the kth distinct element.
@@ -7,11 +9,43 @@
  * You may assume k is always valid, 1 ≤ k ≤ array's length.
  */
 
-// time : O(m + n)
+// time : O(n)
 // space : O(1)
 
 public class KthLargestElementinanArray {
     public int findKthLargest(int[] nums, int k) {
-
+        if (nums == null || nums.length == 0) return 0;
+        int left = 0, right = nums.length -1;
+        while (true){
+            int pos = partition(nums, left, right);
+            if(pos + 1 == k){
+                return nums[pos];
+            }else if(pos + 1 > k){
+                right = pos - 1;
+            }else{
+                left = pos + 1;
+            }
+        }
     }
+    public int partition(int[] nums, int left, int right){
+        int pivot = nums[l];
+        int l = left + 1;
+        int r = right;
+        while(l <= r){
+            if (nums[l] < pivot && nums[r] > pivot){
+                swap(nums, l++, r--);
+            }
+            if (nums[l] >= pivot) return l++;
+            if (nums[r] <= pivot) return r--;
+        }
+        swap(nums, left, r);
+        return r;
+    }
+
+    public void swap(int[] nums, int left, int right){
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right]= temp;
+    }
+
 }
