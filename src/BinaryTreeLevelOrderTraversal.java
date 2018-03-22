ae88f3253df889d478e03c61194ab59e8cf97ef9
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-22 16:59:07 -0500

    add new solution

--- src/BinaryTreeLevelOrderTraversal.java
+++ src/BinaryTreeLevelOrderTraversal.java
@@ -21,8 +21,8 @@ import java.util.Queue;
  * ]
  */
 
-// time : O(n)
-// space : O(n)
+// time : O(n) / O(n)
+// space : O(n) / O(n)
 
 public class BinaryTreeLevelOrderTraversal {
     public List<List<Integer>> levelOrder(TreeNode root) {
@@ -44,5 +44,26 @@ public class BinaryTreeLevelOrderTraversal {
             res.add(list);
         }
         return res;
+    }
+}
+
+
+public class BinaryTreeLevelOrderTraversal2 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        helper(res, root, 0);
+        return res;
+    }
+
+    public void helper(List<List<Integer>> res, TreeNode root, int level){
+        if (root == null) return;
+        if (level >= res.size()){
+            res.add(new ArrayList<>());
+        }
+        res.get(level).add(root.val);
+        helper(res, root.left, level + 1);
+        helper(res, root.right, level + 1);
+
     }
 }
