73f3154ccf4794fc92d9da35cbfaa956e8972ec0
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-24 18:30:04 -0500

    add new solution

--- src/FlattenBinaryTreetoLinkedList.java
+++ src/FlattenBinaryTreetoLinkedList.java
@@ -1,2 +1,32 @@
+/**
+ * Given a binary tree, flatten it to a linked list in-place.
+ * For example,
+ * Given
+ *      1
+ *     / \
+ *    2   5
+ *   / \   \
+ *  3   4   6
+ *  The flattened tree should look like:
+ *  1
+ *   \
+ *    2
+ *     \
+ *      3
+ *       \
+ *        4
+ *         \
+ *          5
+ *           \
+ *            6
+ *
+ */
+
+// time: O()
+// space: O()
+
 public class FlattenBinaryTreetoLinkedList {
+    public void flatten(TreeNode root) {
+
+    }
 }
