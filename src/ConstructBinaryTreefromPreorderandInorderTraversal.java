b22d73c5e3eb143083f2b15990cc1b771a5bc15b
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-24 18:25:42 -0500

    add new solution

--- src/ConstructBinaryTreefromPreorderandInorderTraversal.java
+++ src/ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -1,3 +1,5 @@
+import javax.swing.tree.TreeNode;
+
 /**
  * Given preorder and inorder traversal of a tree, construct the binary tree.
  * Note:
@@ -15,13 +17,29 @@
  */
 
 
-// time : O(2^n)
+// time : O(n)
 // space : O(n)
 
 
 public class ConstructBinaryTreefromPreorderandInorderTraversal {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return helper(0,0, inorder.length -1, preorder, inorder);
 
+    }
 
+    public TreeNode helper(int prestart, int instart, int inend, int[] preorder, int[] inorder){
+        if(prestart > preorder.length -1 || instart > inend){
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[prestart]);
+        int inindex = 0;
+        for (int i = instart; i <= inend; i++){
+            if (inorder[i] == root.val){
+                inindex = i;
+            }
+        }
+        root.left = helper(prestart + 1, instart, inindex -1, preorder, inorder);
+        root.right = helper(prestart + inindex - instart +1, inindex + 1, inend, preorder, inorder);
+        return root;
     }
 }
