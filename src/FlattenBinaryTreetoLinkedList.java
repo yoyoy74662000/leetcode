0b56152641e09ee3760a9e1726064b302ed39fbf
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-24 19:48:18 -0500

    add new solution

--- src/FlattenBinaryTreetoLinkedList.java
+++ src/FlattenBinaryTreetoLinkedList.java
@@ -22,11 +22,18 @@
  *
  */
 
-// time: O()
-// space: O()
+// time: O(n)
+// space: O(n)
 
 public class FlattenBinaryTreetoLinkedList {
+    public TreeNode prev = null;
     public void flatten(TreeNode root) {
-
+        if (root == null) return;
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
     }
+
 }
