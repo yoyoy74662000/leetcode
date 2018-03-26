0b968678a6c5e1be0c0da357a1e372bbdfc26023
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-25 22:33:25 -0500

    add new solution

--- src/LowestCommonAncestorofaBinaryTree.java
+++ src/LowestCommonAncestorofaBinaryTree.java
@@ -29,7 +29,7 @@ public class LowestCommonAncestorofaBinaryTree {
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-        if (root.left != null && root.right != null){
+        if (left != null && right != null){
             return root;
         }
         return left == null ? right : left;
