5712206ee4a1abbfa31b387e8f879a3fe2ec13a3
yoyoy74662000 <yoyoy74662000@github.com>
2018-03-30 14:44:56 -0500

    add new solution

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -12,24 +12,18 @@ import java.util.Stack;
 
 public class ValidParentheses {
     public boolean isValid(String s) {
-        if (s == null || s.length() == 0) return true;
+        if(s == null || s.length() == 0) return true;
         Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()){
-            if (c =='('){
+        for(Character c : s.toCharArray()){
+            if (c == '(')
                 stack.push(')');
-            }
-            else if (c == '['){
-                stack.push(']');
-            }
-            else if (c == '{'){
+            else if (c == '{')
                 stack.push('}');
-            }
-            else {
-                if (stack.isEmpty() || stack.pop() != c)
-                    return false;
-            }
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
         }
         return stack.isEmpty();
-
     }
 }
