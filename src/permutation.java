6f5ed7a2859dc285c096be5f5c41210e944bd484
yoyoy74662000 <yoyoy74662000@github.com>
2018-04-03 16:15:33 -0500

    add new solution

---
+++ src/permutation.java
@@ -0,0 +1,41 @@
+/**
+ * Given a collection of distinct numbers, return all possible permutations.
+ * For example,
+ * [1,2,3] have the following permutations:
+ [
+ [1,2,3],
+ [1,3,2],
+ [2,1,3],
+ [2,3,1],
+ [3,1,2],
+ [3,2,1]
+ ]
+ */
+
+// time: O()
+// space: O(n)
+
+public class permutation {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        if(nums == null || nums.length == 0) return res;
+        helper(res, list, nums);
+        return res;
+    }
+
+    public static void helper(List<List<Integer>> res, List<Integer> list, int[] nums){
+        if(list.size() == nums.length){
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(list.contains(nums[i])) continue;
+            list.add(nums[i]);
+            helper(res, list, nums);
+            list.remove(list.size() - 1);
+        }
+
+
+    }
+}
