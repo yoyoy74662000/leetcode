c8ba71abc0d14889fd79869c863eb6ae66dc5576
yoyoy74662000 <yoyoy74662000@github.com>
2018-04-11 17:43:09 -0500

    add new solution

--- src/MergeSortedArray.java
+++ src/MergeSortedArray.java
@@ -15,12 +15,12 @@ public class MergeSortedArray {
         int j = n -1;
         int k = m + n -1;
         while(i >=0 && j >= 0){
-            nums1[k] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
-            k--;
+            nums1[k--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+
         }
         while(j >= 0){
-            nums1[k] = nums2[j];
-            k--;
+            nums1[k--] = nums2[j];
+
         }
     }
 }
