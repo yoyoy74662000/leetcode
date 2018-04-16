9d5316c6410d335d4c0cfdbd25fe51a5e75e4616
yoyoy74662000 <yoyoy74662000@github.com>
2018-04-16 14:31:48 -0500

    add new solution

--- src/SymmetricTree.java
+++ src/SymmetricTree.java
@@ -15,15 +15,15 @@
 public class SymmetricTree {
     public boolean isSymmetric(TreeNode root) {
         if(root == null) return true;
-        return helper(root.left, root.right);
+        return symmetric(root.left, root.right);
     }
 
-    public static boolean helper(TreeNode p, TreeNode q){
+    public static boolean symmetric(TreeNode p, TreeNode q){
         if(p == null && q == null) return true;
-        if(p == null || q == null) return false;
+        if((p == null && q != null) || (p != null && q == null)) return false;
         if(p.val != q.val) return false;
         else{
-            return helper(p.left, q.right) && helper(p.right, q.left);
+            return symmetric(p.left, q.right) && symmetric(p.right, q.left);
         }
     }
 }
