8dd4b2c827b71a42475ebf3310593b3cdabe280f
yoyoy74662000 <yoyoy74662000@github.com>
2018-04-17 17:00:14 -0500

    add new solution

--- src/MinimumDepthofBinaryTree.java
+++ src/MinimumDepthofBinaryTree.java
@@ -2,7 +2,12 @@
  *
  * Given a binary tree, find its minimum depth.
  * The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
- *
+ *      1      2
+       / \
+  1   4   5    2
+         / \
+        2   6  1
+
  */
 
 // time : O(n)
--- src/PathSum.java
+++ src/PathSum.java
@@ -25,9 +25,5 @@ public class PathSum {
             return sum == root.val;
         }
         return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
-
-
-
-
     }
 }
