503360e27822d983f261b25e685aeeddeb14b5f5
yoyoy74662000 <yoyoy74662000@github.com>
2018-04-21 14:50:44 -0500

    add new solution

--- src/SumRoottoLeafNumbers.java
+++ src/SumRoottoLeafNumbers.java
@@ -25,19 +25,20 @@
 
 public class SumRoottoLeafNumbers {
     public int sumNumbers(TreeNode root) {
-        return helper(root , 0);
+        return helper(root, 0);
     }
 
-    public static int helper(TreeNode root, int sum){
-        if(root == null) return 0;
+    public static int helper(TreeNode root, int sum) {
+        if (root == null) return 0;
 
         sum = sum * 10 + root.val;
 
-        if(root.left == null && root.right == null){
+        if (root.left == null && root.right == null) {
             return sum;
         }
         int left = helper(root.left, sum);
         int right = helper(root.right, sum);
 
         return left + right;
+    }
 }
