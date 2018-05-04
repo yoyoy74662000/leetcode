b28074e6517e43f7caa51fe55374cdf55cbd19fd
yoyoy74662000 <yoyoy74662000@github.com>
2018-05-03 21:08:38 -0500

    add new solution

---
+++ src/ReorderList.java
@@ -0,0 +1,59 @@
+/**
+ * Given a singly linked list L: L0→L1→…→Ln-1→Ln,
+ reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
+
+ You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+ Example 1:
+
+ Given 1->2->3->4, reorder it to 1->4->2->3.
+
+ Example 2:
+
+ Given 1->2->3->4->5, reorder it to 1->5->2->4->3.
+ *
+ */
+
+// time: O(n)
+// space: O(1)
+
+public class ReorderList {
+    public void reorderList(ListNode head) {
+        if(head == null) return;
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode l1 = head;
+        ListNode temp = null;
+        while(fast != null && fast.next != null){
+            temp = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        temp.next = null;
+        ListNode l2 = reverse(slow);
+        merge(l1,l2);
+    }
+
+    public static ListNode reverse(ListNode head){
+        ListNode prev = null;
+        while(head != null){
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+
+    public static void merge(ListNode l1, ListNode l2){
+        while(l1 != null || l2 != null){
+            ListNode n1 = l1.next;
+            ListNode n2 = l2.next;
+            l1.next = l2;
+            if(n1 == null) break;
+            l2.next = n1;
+            l1 = n1;
+            l2 = n2;
+        }
+    }
+}
