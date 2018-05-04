37fe213b5d8213b6f1d08df9cb21d2e1b3b1626a
yoyoy74662000 <yoyoy74662000@github.com>
2018-05-04 18:45:21 -0500

    add new solution

--- src/MaximumProductSubarray.java
+++ src/MaximumProductSubarray.java
@@ -23,7 +23,7 @@ public class MaximumProductSubarray {
         int max = nums[0];
         int min = nums[0];
         int res = nums[0];
-        for (int i = 1; i < nums.length -1; i++){
+        for (int i = 1; i < nums.length; i++){
             int temp = max;
             max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
             min = Math.min(Math.min(temp * nums[i], min * nums[i]), nums[i]);
