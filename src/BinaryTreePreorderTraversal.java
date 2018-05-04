a51b794aa74bad73adfa54e7f5a41ff2c7103a28
yoyoy74662000 <yoyoy74662000@github.com>
2018-05-04 14:35:17 -0500

    add new solution

---
+++ src/BinaryTreePreorderTraversal.java
@@ -0,0 +1,45 @@
+/**
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ Example:
+ Input: [1,null,2,3]
+    1
+     \
+      2
+     /
+    3
+
+ Output: [1,2,3]
+ Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+
+// time: O(n) / O(n)
+// space: O(n) / O(n)
+
+public class BinaryTreePreorderTraversal {
+    //iterative
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null) return null;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode cur = stack.pop();
+            if(cur.right != null) stack.push(cur.right);
+            if(cur.left != null) stack.push(cur.left);
+            res.add(cur.val);
+        }
+        return res;
+    }
+}
+
+public class BinaryTreePreorderTraversal2 {
+    //recursion
+    List<Integer> res = new ArrayList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if(root == null) return res;
+        if(root != null) res.add(root.val);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+        return res;
+    }
+}
