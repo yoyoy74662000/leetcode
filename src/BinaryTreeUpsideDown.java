980299c0b838f763850c724858c41beb27f0892d
yoyoy74662000 <yoyoy74662000@github.com>
2018-05-05 20:51:06 -0500

    add new solution

---
+++ src/BinaryTreeUpsideDown.java
@@ -0,0 +1,29 @@
+/**
+ * Binary Tree Upside Down
+ * Given a binary tree with a specific structure - all right nodes are either:
+ * 1. leaf nodes with a left sibling or 2. empthy.
+ * Reverse it into a new tree where the origin al right nodes
+ * becoming new left leaf nodes. Return the new root.
+ *   1                4
+    /  \             / \
+   2    3   ->      5   2
+  / \                  / \
+ 4   5                3   1
+ *
+ *
+ */
+public class BinaryTreeUpsideDown {
+    public TreeNode upsideDownBinaryTree(TreeNode root){
+        if(root == null || root.left == null & root.right == null){
+            return root;
+        }
+
+        TreeNode newRoot = upsideDownBinaryTree(root.left);
+        root.left.left = root.right;
+        root.left.right = root;
+
+        root.left = null;
+        root.right = null;
+        return newRoot;
+    }
+}
