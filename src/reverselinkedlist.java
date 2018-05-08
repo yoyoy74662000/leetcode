af71d8e21a294dc857da1699fbdb4b79d95c7890
yoyoy74662000 <yoyoy74662000@github.com>
2018-05-07 20:54:10 -0500

    add new solution

--- src/reverselinkedlist.java
+++ src/reverselinkedlist.java
@@ -9,6 +9,8 @@
 // time : O(n)
 // space : O(n)
 
+
+//recursion
 public class reverselinkedlist {
     public ListNode reverseList(ListNode head) {
         if(head == null || head.next == null) return head;
@@ -17,5 +19,37 @@ public class reverselinkedlist {
         head.next.next = head;
         head.next = null;
         return root;
+    }
+}
+
+//recursion
+public class reverselinkedlist2 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        //get second node
+        ListNode second = head.next;
+        //set first's next to be null
+        head.next = null;
+
+        ListNode rest = reverseList(second);
+        second.next = head;
+
+        return rest;
+    }
+}
+//iteration
+public class reverselinkedlist2 {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        if (head == null || head.next == null) return head;
+        while(head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
     }
 }
