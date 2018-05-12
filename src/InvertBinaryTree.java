919c77c5df3075fc32ae570e8b57b72ed5f37589
yoyoy74662000 <yoyoy74662000@github.com>
2018-05-12 14:01:00 -0500

    add new solution

--- src/InvertBinaryTree.java
+++ src/InvertBinaryTree.java
@@ -21,8 +21,8 @@
  *
  */
 
-//
-//
+// time: O(n)
+// space: O(n)
 
 public class InvertBinaryTree {
     public TreeNode invertTree(TreeNode root) {
@@ -33,6 +33,28 @@ public class InvertBinaryTree {
         TreeNode right = invertTree(root.right);
         root.left = right;
         root.right = left;
+        return root;
+    }
+}
+
+public class InvertBinaryTree2 {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return root;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                TreeNode cur = queue.poll();
+                TreeNode temp = cur.left;
+                cur.left = cur.right;
+                cur.right = temp;
+                if(cur.left != null) queue.offer(cur.left);
+                if(cur.right != null) queue.offer(cur.right);
+            }
+
+
+        }
         return root;
     }
 }
