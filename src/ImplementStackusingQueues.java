4304f3fcc15dbb053797a5ce6fac0780a54c3cc9
yoyoy74662000 <yoyoy74662000@github.com>
2018-05-12 14:04:28 -0500

    add new solution

---
+++ src/ImplementStackusingQueues.java
@@ -0,0 +1,29 @@
+public class ImplementStackusingQueues {
+    private Queue<Integer> queue;
+    /** Initialize your data structure here. */
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+
+    /** Push element x onto stack. */
+    public void push(int x) {
+        queue.add(x);
+        for (int i=1; i<queue.size(); i++)
+            queue.add(queue.remove());
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return queue.remove();
+    }
+
+    /** Get the top element. */
+    public int top() {
+        return queue.peek();
+    }
+
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
