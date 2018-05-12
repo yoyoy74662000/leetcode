5de3cb4f5760f5654454084acb75428d54e90d9d
yoyoy74662000 <yoyoy74662000@github.com>
2018-05-12 14:38:16 -0500

    add new solution

---
+++ src/ImplementQueueusingStacks.java
@@ -0,0 +1,32 @@
+public class ImplementQueueusingStacks {
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    public void push(int x) {
+        s1.push(x);
+    }
+
+    public int pop() {
+        if(!s2.isEmpty()) return s2.pop();
+        else{
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+            return s2.pop();
+        }
+    }
+
+    public int peek() {
+        if(!s2.isEmpty()) return s2.peek();
+        else{
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+            return s2.peek();
+        }
+    }
+
+    public boolean empty() {
+        return s1.isEmpty() && s2.isEmpty();
+    }
+}
