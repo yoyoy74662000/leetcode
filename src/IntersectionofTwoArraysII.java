b96edef1f1a5fe935c83280a354350b26d37e5fe
yoyoy74662000 <yoyoy74662000@github.com>
2018-05-18 21:39:17 -0500

    add new solution

---
+++ src/IntersectionofTwoArraysII.java
@@ -0,0 +1,46 @@
+/**
+ *  Given two arrays, write a function to compute their intersection.
+
+ Example:
+ Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
+
+ Note:
+
+ Each element in the result should appear as many times as it shows in both arrays.
+ The result can be in any order.
+
+ */
+
+// time : O(n)
+// space : O(n)
+
+public class IntersectionofTwoArraysII {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if(nums1 == null || nums2 == null) return new int[]{};
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        List<Integer> intersection = new ArrayList<>();
+
+        for(int num : nums1){
+            if(map.containsKey(num)){
+                map.put(num, map.get(num)+1);
+            }else{
+                map.put(num,1);
+            }
+        }
+        for(int num : nums2){
+            if(map.containsKey(num)){
+                if(map.get(num) > 0){
+                    intersection.add(num);
+                    map.put(num, map.get(num)-1);
+                }
+            }
+        }
+        int[] result = new int[intersection.size()];
+        int k = 0;
+        for(int num : intersection){
+            result[k++] = num;
+        }
+
+        return result;
+    }
+}
