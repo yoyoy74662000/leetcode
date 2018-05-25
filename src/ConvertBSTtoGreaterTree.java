3af4bbb0f0f56cbc9a898b994ae10b316c83bebd
yoyoy74662000 <yoyoy74662000@github.com>
2018-05-25 16:12:23 -0500

    add new solution

---
+++ src/ConvertBSTtoGreaterTree.java
@@ -0,0 +1,32 @@
+/**
+ * Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus sum of all keys greater than the original key in BST.
+
+ Example:
+
+ Input: The root of a Binary Search Tree like this:
+        5
+      /   \
+     2     13
+
+ Output: The root of a Greater Tree like this:
+        18
+      /   \
+    20     13
+ *
+ */
+
+
+// time: O(n)
+// space: O(n)
+
+public class ConvertBSTtoGreaterTree {
+    public int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        if(root == null) return null;
+        convertBST(root.right);
+        sum = sum + root.val;
+        root.val = sum;
+        convertBST(root.left);
+        return root;
+    }
+}
