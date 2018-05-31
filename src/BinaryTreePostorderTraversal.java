60557f14b25fb7f365d3d16871c097fae5ff10b6
yoyoy74662000 <yoyoy74662000@github.com>
2018-05-31 14:24:56 -0500

    add new solution

--- src/BinaryTreePostorderTraversal.java
+++ src/BinaryTreePostorderTraversal.java
@@ -37,3 +37,24 @@ public class BinaryTreePostorderTraversal {
         return res;
     }
 }
+
+public class BinaryTreePostorderTraversal2 {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> ans = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        if (root == null) return ans;
+
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            ans.addFirst(cur.val);
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+        }
+        return ans;
+    }
+}
