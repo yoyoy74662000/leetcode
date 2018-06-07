5fe97f48ad26ff2350fda69a4f99796c0dc62607
yoyoy74662000 <yoyoy74662000@github.com>
2018-06-06 23:28:09 -0500

    add new solution

--- src/ContainsDuplicate.java
+++ src/ContainsDuplicate.java
@@ -38,3 +38,21 @@ public class ContainsDuplicate2 {
         return false;
     }
 }
+
+public class ContainsDuplicate3 {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i], map.get(nums[i]) + 1);
+            }else{
+                map.put(nums[i], 1);
+            }
+
+            if(map.get(nums[i]) >= 2){
+                return true;
+            }
+        }
+        return false;
+    }
+}
