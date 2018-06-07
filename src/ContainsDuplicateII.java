1bf1cba9d97334980a941baa91bbb4039ef62f8c
yoyoy74662000 <yoyoy74662000@github.com>
2018-06-06 23:43:19 -0500

    add new solution

--- src/ContainsDuplicateII.java
+++ src/ContainsDuplicateII.java
@@ -36,3 +36,18 @@ public class ContainsDuplicateII {
         return false;
     }
 }
+
+public class ContainsDuplicateII2 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        if (nums.length <= 1) return false;
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i]) && (i - map.get(nums[i]) <= k)){
+                return true;
+            }else{
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+}
