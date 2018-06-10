0d38f8c04f583d01fcd8db9ea86395a5086b1906
yoyoy74662000 <yoyoy74662000@github.com>
2018-06-10 17:42:43 -0500

    add new solution

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,3 +1,4 @@
+
 /**
  * Write a function that takes a string as input and returns the string reversed.
 
@@ -24,5 +25,25 @@ public class ReverseString {
             str[str.length-i-1] = temp;       //後面變前面
         }
         return String.valueOf(str);
+    }
+}
+
+public class ReverseString2 {
+    public String reverseString(String s) {
+        if(s == null || s.length() == 0) return"";
+
+        return swap(s);
+    }
+
+    public String swap(String s){
+        char[] r = s.toCharArray();
+        int right = s.length()-1;
+        for(int left = 0; left < s.length()/2; left++){
+            char temp = r[left];
+            r[left] = r[right];
+            r[right] = temp;
+            right--;
+        }
+        return String.valueOf(r);
     }
 }
