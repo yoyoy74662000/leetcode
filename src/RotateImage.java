c08984ace33ae36e2a7a384865ff8365c1c0350d
yoyoy74662000 <yoyoy74662000@github.com>
2018-06-18 21:53:02 -0500

    add new solution

--- src/RotateImage.java
+++ src/RotateImage.java
@@ -35,8 +35,8 @@ public class RotateImage {
         for (int i = 0; i < matrix.length; i++){
             for (int j = 0; j < matrix.length / 2; j++){
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][matrix.length- 1 - j];
-                matrix[i][matrix.length- 1 - j] = temp;
+                matrix[i][j] = matrix[i][matrix[0].length-1-j];
+                matrix[i][matrix[0].length-1-j] = temp;
             }
         }
     }
