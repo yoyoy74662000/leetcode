27db112fc24e3bfbba0f86d3a90212c28f2cb8a3
yoyoy74662000 <yoyoy74662000@github.com>
2018-07-14 21:09:58 -0500

    add new solution

--- src/RotateImage.java
+++ src/RotateImage.java
@@ -25,6 +25,7 @@ public class RotateImage {
     public void rotate(int[][] matrix) {
         // 先斜對角
         for (int i = 0; i < matrix.length; i++){
+            //j 之所以不能等於 0 因為會對稱回去
             for (int j = i; j < matrix[0].length; j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
