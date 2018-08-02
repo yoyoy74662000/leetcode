12b229b31a011b87cdffaf61e34790cf8fc8267b
yoyoy74662000 <yoyoy74662000@github.com>
2018-08-02 18:24:09 -0500

    add new solution

--- src/MedianofTwoSortedArrays.java
+++ src/MedianofTwoSortedArrays.java
@@ -1,11 +1,48 @@
 /**
- * time :O(m+n)
+ * time :O(m+n) /Olog((m+n))
  * space :O(n)
  *
- * don't know O(log(m+n))
+ *
  *
  */
+
 public class MedianofTwoSortedArrays {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int len = nums1.length + nums2.length;
+        if(len % 2 == 1) {
+            return (findKth(nums1, 0, nums2, 0, len / 2 + 1));
+        }
+
+        return (findKth(nums1, 0, nums2, 0, len / 2) + findKth(nums1, 0, nums2, 0, len / 2 + 1)) / 2.0;
+    }
+
+    private static int findKth(int[] A, int A_start,
+                               int[] B, int B_start,
+                               int k) {
+        if(A_start >= A.length) {
+            return B[B_start + k - 1];
+        }
+
+        if(B_start >= B.length) {
+            return A[A_start + k - 1];
+        }
+
+        if(k == 1) {
+            return Math.min(A[A_start], B[B_start]);
+        }
+
+        int A_key = A_start + k / 2 - 1 < A.length ? A[A_start + k / 2 - 1] : Integer.MAX_VALUE;
+        int B_key = B_start + k / 2 - 1 < B.length ? B[B_start + k / 2 - 1] : Integer.MAX_VALUE;
+
+        if(A_key < B_key) {
+            return findKth(A, A_start + k / 2, B, B_start, k - k / 2);
+        } else {
+            return findKth(A, A_start, B, B_start + k / 2, k - k / 2);
+        }
+    }
+}
+
+public class MedianofTwoSortedArrays2 {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int i = 0, nums1Size = nums1.length;  //  setting pointer for first array
         int j = 0, nums2Size = nums2.length;    //  setting pointer for second array
