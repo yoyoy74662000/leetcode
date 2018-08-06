961d6cd2e24049e3f76978bcdd79cc83410e3b15
yoyoy74662000 <yoyoy74662000@github.com>
2018-08-06 17:28:53 -0500

    add new solution

--- src/MergeSortedArray.java
+++ src/MergeSortedArray.java
@@ -3,6 +3,13 @@
  * Note:
  * You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
  * The number of elements initialized in nums1 and nums2 are m and n respectively.
+ * Example:
+
+ Input:
+ nums1 = [1,2,3,0,0,0], m = 3
+ nums2 = [2,5,6],       n = 3
+
+ Output: [1,2,2,3,5,6]
  */
 
 // time : O(m + n)
