5000b1002ac2f10ca835d6335dc5e6f823083466
yoyoy74662000 <yoyoy74662000@github.com>
2018-08-07 16:28:22 -0500

    add new solution

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -10,7 +10,7 @@ public class LongestCommonPrefix {
         String res = strs[0];
         for (int i = 1; i < strs.length; i++){
             while(strs[i].indexOf(res) != 0){
-                res = res.substring(0, res.length-1);
+                res = res.substring(0, res.length()-1);
             }
         }
         return res;
