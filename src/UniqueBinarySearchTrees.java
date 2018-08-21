71a1dee9330df4af78e3b1c569bc968ba83f56b2
yoyoy74662000 <yoyoy74662000@github.com>
2018-08-21 00:40:22 -0500

    add new solution

--- src/UniqueBinarySearchTrees.java
+++ src/UniqueBinarySearchTrees.java
@@ -23,8 +23,7 @@ public class UniqueBinarySearchTrees {
     public int numTrees(int n) {
         int[] count = new int[n + 1];
         count[0] = 1;
-        count[1] = 1;
-        for (int i = 2; i < n + 1; i++) {
+        for (int i = 1; i <= n; i++) {
             for (int j = 0; j < i; j++) {
                 count[i] += count[j] * count[i - j - 1];
             }
