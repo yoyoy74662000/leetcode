f8d93ea41854c6730e63e9e90eb3b457d08044cd
yoyoy74662000 <yoyoy74662000@github.com>
2018-09-08 22:06:22 -0500

    add new solution

--- src/SortList.java
+++ src/SortList.java
@@ -5,6 +5,43 @@
 // time : O(nlogn)
 // space : O(1)
 
+public class SortList3 {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode mid = getmid(head);
+        ListNode next = mid.next;
+        mid.next = null;
+        return merge(sortList(head), sortList(next));
+    }
+
+    public static ListNode getmid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static ListNode merge(ListNode a, ListNode b) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (a != null && b != null) {
+            if (a.val <= b.val) {
+                cur.next = a;
+                a = a.next;
+            } else {
+                cur.next = b;
+                b = b.next;
+            }
+            cur = cur.next;
+        }
+        if (a == null) cur.next = b;
+        else cur.next = a;
+        return dummy.next;
+    }
+}
 
 public class SortList1 {
     public ListNode sortList(ListNode head) {
@@ -97,42 +134,5 @@ public class SortList2 {
 
         return dummy.next;
 
-    }
-}
-public class SortList3 {
-    public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null) return head;
-        ListNode mid = getmid(head);
-        ListNode next = mid.next;
-        mid.next = null;
-        return merge(sortList(head), sortList(next));
-    }
-
-    public static ListNode getmid(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        return slow;
-    }
-
-    public static ListNode merge(ListNode a, ListNode b) {
-        ListNode dummy = new ListNode(0);
-        ListNode cur = dummy;
-        while (a != null && b != null) {
-            if (a.val <= b.val) {
-                cur.next = a;
-                a = a.next;
-            } else {
-                cur.next = b;
-                b = b.next;
-            }
-            cur = cur.next;
-        }
-        if (a == null) cur.next = b;
-        else cur.next = a;
-        return dummy.next;
     }
 }
