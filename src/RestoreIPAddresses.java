546f0d77d5fdb296432d80555bf1ce3c3f7f607f
yoyoy74662000 <yoyoy74662000@github.com>
2018-09-23 15:18:04 -0500

    add new solution

---
+++ src/RestoreIPAddresses.java
@@ -0,0 +1,29 @@
+/**
+ * Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+
+ Example:
+
+ Input: "25525511135"
+ Output: ["255.255.11.135", "255.255.111.35"]
+ *
+ *
+ */
+public class RestoreIPAddresses {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> solutions = new ArrayList<String>();
+        restoreIp(s, solutions, 0, "", 0);
+        return solutions;
+    }
+
+    private void restoreIp(String ip, List<String> solutions, int idx, String restored, int count) {
+        if (count > 4) return;
+        if (count == 4 && idx == ip.length()) solutions.add(restored);
+
+        for (int i=1; i<4; i++) {
+            if (idx+i > ip.length()) break;
+            String s = ip.substring(idx,idx+i);
+            if ((s.startsWith("0") && s.length()>1) || (i==3 && Integer.parseInt(s) >= 256)) continue;
+            restoreIp(ip, solutions, idx+i, restored+s+(count==3?"" : "."), count+1);
+        }
+    }
+}
