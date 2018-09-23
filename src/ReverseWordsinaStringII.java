1a7812c951564023eb4a58f0cf2ead2a4bf3941c
yoyoy74662000 <yoyoy74662000@github.com>
2018-09-23 16:56:03 -0500

    add new solution

---
+++ src/ReverseWordsinaStringII.java
@@ -0,0 +1,38 @@
+/**
+ * Given an input string , reverse the string word by word.
+
+ Example:
+
+ Input:  ["t","h","e"," ","s","k","y"," ","i","s"," ","b","l","u","e"]
+ Output: ["b","l","u","e"," ","i","s"," ","s","k","y"," ","t","h","e"]
+ *
+ *
+ */
+public class ReverseWordsinaStringII {
+    public void reverseWords(char[] s) {
+        // Three step to reverse
+        // 1, reverse the whole sentence
+        reverse(s, 0, s.length - 1);
+        // 2, reverse each word
+        int start = 0;
+        int end = -1;
+        for (int i = 0; i < s.length; i++) {
+            if (s[i] == ' ') {
+                reverse(s, start, i - 1);
+                start = i + 1;
+            }
+        }
+        // 3, reverse the last word, if there is only one word this will solve the corner case
+        reverse(s, start, s.length - 1);
+    }
+
+    public void reverse(char[] s, int start, int end) {
+        while (start < end) {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
