5cde7e5b96d3e33ba5419cdbba0270d1377a7f16
yoyoy74662000 <yoyoy74662000@github.com>
2018-09-28 18:29:43 -0500

    add new solution

---
+++ src/WordBreak.java
@@ -0,0 +1,35 @@
+/**
+ * Example 1:
+
+ Input: s = "leetcode", wordDict = ["leet", "code"]
+ Output: true
+ Explanation: Return true because "leetcode" can be segmented as "leet code".
+ Example 2:
+
+ Input: s = "applepenapple", wordDict = ["apple", "pen"]
+ Output: true
+ Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+ Note that you are allowed to reuse a dictionary word.
+ Example 3:
+
+ Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ Output: false
+ *
+ */
+public class WordBreak {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] f = new boolean[s.length() + 1];
+
+        f[0] = true;
+        for(int i=1; i <= s.length(); i++){
+            for(int j=0; j < i; j++){
+                if(f[j] && wordDict.contains(s.substring(j, i))){
+                    f[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return f[s.length()];
+    }
+}
