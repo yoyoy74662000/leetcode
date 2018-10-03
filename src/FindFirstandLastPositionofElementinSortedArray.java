d58f73c956e80d27b70d3bb6a60479b8e6d055db
yoyoy74662000 <yoyoy74662000@github.com>
2018-10-03 13:20:16 -0500

    add new solution 10/03

--- src/FindFirstandLastPositionofElementinSortedArray.java
+++ src/FindFirstandLastPositionofElementinSortedArray.java
@@ -1,20 +1,18 @@
 /**
  * Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
-
- Your algorithm's runtime complexity must be in the order of O(log n).
-
- If the target is not found in the array, return [-1, -1].
-
- Example 1:
-
- Input: nums = [5,7,7,8,8,10], target = 8
- Output: [3,4]
- Example 2:
-
- Input: nums = [5,7,7,8,8,10], target = 6
- Output: [-1,-1]
- *
- *
+ * <p>
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ * <p>
+ * If the target is not found in the array, return [-1, -1].
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [5,7,8,8,10,10], target = 8
+ * Output: [3,4]
+ * Example 2:
+ * <p>
+ * Input: nums = [5,7,7,8,8,10], target = 6
+ * Output: [-1,-1]
  */
 public class FindFirstandLastPositionofElementinSortedArray {
     public int[] searchRange(int[] nums, int target) {
@@ -23,48 +21,50 @@ public class FindFirstandLastPositionofElementinSortedArray {
         result[1] = findLastPostion(nums, target);
         return result;
     }
+
     public int findFirstPostion(int[] nums, int target) {
-        if(nums == null || nums.length == 0) return -1;
-        int start = 0, end = nums.length -1;
-        while (start + 1 < end){
-            int mid = (end - start ) / 2 + start;
-            if (nums[mid] == target){
+        if (nums == null || nums.length == 0) return -1;
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = (end - start) / 2 + start;
+            if (nums[mid] == target) {
                 end = mid;
-            }else if(nums[mid] > target){
+            } else if (nums[mid] > target) {
                 end = mid;
-            }else{
+            } else {
                 start = mid;
             }
         }
 
-
-        if(nums[start] == target) {
+        // 差在這裡的順序
+        if (nums[start] == target) {
             return start;
         }
-        if(nums[end] == target) {
+        if (nums[end] == target) {
             return end;
         }
         return -1;
     }
 
     public int findLastPostion(int[] nums, int target) {
-        if(nums == null || nums.length == 0) return -1;
-        int start = 0, end = nums.length -1;
-        while (start + 1 < end){
-            int mid = (end - start ) / 2 + start;
-            if (nums[mid] == target){
+        if (nums == null || nums.length == 0) return -1;
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = (end - start) / 2 + start;
+            if (nums[mid] == target) {
                 start = mid;
-            }else if(nums[mid] > target){
+            } else if (nums[mid] > target) {
                 end = mid;
-            }else{
+            } else {
                 start = mid;
             }
         }
-        if(nums[end] == target) {
+        // 差在這裡的順序
+        if (nums[end] == target) {
             return end;
         }
 
-        if(nums[start] == target) {
+        if (nums[start] == target) {
             return start;
         }
         return -1;
