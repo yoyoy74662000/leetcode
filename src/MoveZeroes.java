cfc2453f5b505f15457554daaa5cd950911c05e5
yoyoy74662000 <yoyoy74662000@github.com>
2018-10-05 13:16:35 -0500

    add new solution 10/05

--- src/MoveZeroes.java
+++ src/MoveZeroes.java
@@ -12,18 +12,17 @@
 
 public class MoveZeroes {
     public void moveZeroes(int[] nums) {
-        int start = 0;
         if(nums == null || nums.length == 0) return;
-        for(int  i = 0; i < nums.length; i++){
-            if(nums[i] == 0){
-                continue;
-            }else{
-                nums[start] = nums[i];
-                start++;
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                nums[count] = nums[i];
+                count++;
             }
         }
-        while(start < nums.length){
-            nums[start++] = 0;
+        while(count < nums.length){
+            nums[count] = 0;
+            count++;
         }
     }
 }
