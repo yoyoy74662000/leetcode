ee2cf0ae40d4839ce0cdea1b1f2b7d275e1740ac
yoyoy74662000 <yoyoy74662000@github.com>
2018-10-05 22:02:48 -0500

    add new solution 10/05

--- src/NumberofIslands.java
+++ src/NumberofIslands.java
@@ -33,21 +33,25 @@ public class NumberofIslands {
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == '1'){
-                    dfs(grid, i, j);
+                    bfs(grid, i, j);
                     res++;
                 }
             }
         }
         return res;
     }
-    public void dfs(char[][] grid, int i, int j){
+//      11000
+//      11000
+//      00100
+//      00011
+    public void bfs(char[][] grid, int i, int j){
         if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') return;
         else{
             grid[i][j] = '0';
-            dfs(grid, i, j + 1);
-            dfs(grid, i, j - 1);
-            dfs(grid, i + 1, j);
-            dfs(grid, i - 1, j);
+            bfs(grid, i, j + 1);
+            bfs(grid, i, j - 1);
+            bfs(grid, i + 1, j);
+            bfs(grid, i - 1, j);
         }
     }
 }
