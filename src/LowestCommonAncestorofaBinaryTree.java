87ae4f5dad4d758d263cab7df4a607009de48db8
yoyoy74662000 <yoyoy74662000@github.com>
2018-10-10 13:01:32 -0500

    add new solution 10/10

--- src/LowestCommonAncestorofaBinaryTree.java
+++ src/LowestCommonAncestorofaBinaryTree.java
@@ -3,21 +3,15 @@
  * According to the definition of LCA on Wikipedia:
  * “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
  *
- *
- *
- *
- *            ______3_______
- *           /              \
- *       ___5__          ___1__
- *      /      \        /      \
- *     6      _2       0       8
- *   /  \
- *  7   4
+ *          ______3_______
+ *         /              \
+ *     ___5__          ___1__
+ *    /      \        /      \
+ *    6      2       0       8
+ *  /  \
+ * 7   4
  * For example, the lowest common ancestor (LCA) of nodes 5 and 1 is 3.
  * Another example is LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
- *
- *
- *
  */
 
 //time : O(n)
@@ -29,10 +23,9 @@ public class LowestCommonAncestorofaBinaryTree {
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
 
-        if (left != null && right != null){
+        if (left != null && right != null) {
             return root;
-        }
-        else {
+        } else {
             return left == null ? right : left;
         }
 
