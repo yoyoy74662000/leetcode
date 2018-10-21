754ca1a86e68c4de6ad75b26b1a99a75eb0aa4ab
yoyoy74662000 <yoyoy74662000@github.com>
2018-10-20 23:01:04 -0500

    add new solution 10/16

--- src/FirstUniqueCharacterinaString.java
+++ src/FirstUniqueCharacterinaString.java
@@ -24,3 +24,24 @@ public class FirstUniqueCharacterinaString {
         return -1;
     }
 }
+public class FirstUniqueCharacterinaString2 {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            Integer count = map.get(c);
+            if (count == null) {
+                map.put(c, 1);
+            } else {
+                map.put(c, count + 1);
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
