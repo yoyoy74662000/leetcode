62ed566184e2b60a66cdbd5bdf6a28a6f3bab129
yoyoy74662000 <yoyoy74662000@github.com>
2018-10-21 00:34:37 -0500

    add new solution 10/20

--- src/InsertDeleteGetRandom.java
+++ src/InsertDeleteGetRandom.java
@@ -41,3 +41,46 @@ public class InsertDeleteGetRandom {
         }
     }
 }
+
+public class InsertDeleteGetRandom2 {
+    HashMap<Integer, Integer> map;
+    ArrayList<Integer> list;
+
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+        map = new HashMap<Integer, Integer>();
+        list = new ArrayList<Integer>();
+    }
+
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if(map.containsKey(val)) {
+            return false;
+        }else {
+            map.put(val, list.size());
+            list.add(val);
+            return true;
+        }
+    }
+
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if(!map.containsKey(val)) {
+            return false;
+        }else {
+            int key = map.get(val);
+            int lastElement = list.get(list.size() - 1);
+            map.put(lastElement, key);
+            list.set(key, lastElement);
+            map.remove(val);
+            list.remove(list.size() - 1);
+            return true;
+        }
+    }
+
+    /** Get a random element from the set. */
+    public int getRandom() {
+        Random random = new Random();
+        return list.get( random.nextInt(list.size()) );
+    }
+}
