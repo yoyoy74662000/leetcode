c55d97031637801b6cfdcbdf3eec69fc837089fd
yoyoy74662000 <yoyoy74662000@github.com>
2018-10-21 15:49:15 -0500

    add new solution 10/20

--- src/TopKFrequentElements.java
+++ src/TopKFrequentElements.java
@@ -5,26 +5,27 @@
  * Note:
  * You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
  * Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
- *
  */
-public class    TopKFrequentElements {
+public class TopKFrequentElements {
     public List<Integer> topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        for(int n: nums){
-            map.put(n, map.getOrDefault(n,0)+1);
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
         }
 
         TreeMap<Integer, List<Integer>> freqMap = new TreeMap<>();
-        for(int num : map.keySet()){
-            int freq = map.get(num);
-            if(!freqMap.containsKey(freq)){
+        for (int num : map.keySet()) {
+            int freq = map.get(num); //從map 得到 frequence 1->3 2->2 3->1
+            if (!freqMap.containsKey(freq)) {
                 freqMap.put(freq, new LinkedList<>());
             }
-            freqMap.get(freq).add(num);
+            freqMap.get(freq).add(num); // 3 [1], 2 [2], 1 [3]
         }
 
         List<Integer> res = new ArrayList<>();
-        while(res.size()<k){
+        while (res.size() < k) {
+            // The pollLastEntry() method is used to remove and returns a key-value mapping associated
+            // with the greatest key in this map, or null if the map is empty.
             Map.Entry<Integer, List<Integer>> entry = freqMap.pollLastEntry();
             res.addAll(entry.getValue());
         }
