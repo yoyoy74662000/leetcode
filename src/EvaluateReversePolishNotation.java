d9b8b811b2c65b689c61bbc15882d091e00fe7c0
yoyoy74662000 <yoyoy74662000@github.com>
2018-10-28 17:50:31 -0500

    add new solution 10/28

--- src/EvaluateReversePolishNotation.java
+++ src/EvaluateReversePolishNotation.java
@@ -1,35 +1,32 @@
 public class EvaluateReversePolishNotation {
     int returnValue = 0;
-
     String operators = "+-*/";
 
     Stack<String> stack = new Stack<String>();
 
-        for(String t : tokens){
-        if(!operators.contains(t)){
+		for (String t : tokens) {
+        if (!operators.contains(t)) { //push to stack if it is a number
             stack.push(t);
-        }else{
+        } else {//pop numbers from stack if it is an operator
             int a = Integer.valueOf(stack.pop());
             int b = Integer.valueOf(stack.pop());
-            int index = operators.indexOf(t);
-            switch(index){
-                case 0:
-                    stack.push(String.valueOf(a+b));
+            switch (t) {
+                case "+":
+                    stack.push(String.valueOf(a + b));
                     break;
-                case 1:
-                    stack.push(String.valueOf(b-a));
+                case "-":
+                    stack.push(String.valueOf(b - a));
                     break;
-                case 2:
-                    stack.push(String.valueOf(a*b));
+                case "*":
+                    stack.push(String.valueOf(a * b));
                     break;
-                case 3:
-                    stack.push(String.valueOf(b/a));
+                case "/":
+                    stack.push(String.valueOf(b / a));
                     break;
             }
         }
     }
 
     returnValue = Integer.valueOf(stack.pop());
-
-        return returnValue;
+    return returnValue;
 }
