a60d70e2237277ec811da1fb8ad4885bd2781ba1
yoyoy74662000 <yoyoy74662000@github.com>
2018-11-01 15:12:25 -0500

    add new solution 11/01

--- src/MergeIntervals.java
+++ src/MergeIntervals.java
@@ -11,23 +11,34 @@
 // space : O(1)
 
 public class MergeIntervals {
-    public List<Interval> merge(List<Interval> intervals) {
+    public static void main(String[] args) {
+        List<Interval> x = new ArrayList<>();
+        x.add(new Interval(1, 3));
+        x.add(new Interval(2, 6));
+        x.add(new Interval(8, 10));
+        x.add(new Interval(15, 18));
+        x.add(new Interval(17, 20));
+        x = merge(x);
+        for(Interval i : x){
+            System.out.println(i.start + " " + i.end);
+        }
+
+
+    }
+    public static List<Interval> merge(List<Interval> intervals) {
         // sort start&end
         int n = intervals.size();
         int[] starts = new int[n];
         int[] ends = new int[n];
         for (int i = 0; i < n; i++) {
-            // 先把 頭尾分出來
             starts[i] = intervals.get(i).start;
             ends[i] = intervals.get(i).end;
         }
         Arrays.sort(starts);
         Arrays.sort(ends);
-        //Input: [[1,3],[2,6],[8,10],[15,18]]
-        //Output: [[1,6],[8,10],[15,18]]
-        // start 1,2,8,15
-        // end   3,6,10,18
         // loop through
+        // 1 2 8 15
+        // 3 6 10 18
         List<Interval> res = new ArrayList<Interval>();
         for (int i = 0, j = 0; i < n; i++) { // j is start of interval.
             if (i == n - 1 || starts[i + 1] > ends[i]) {
@@ -36,5 +47,12 @@ public class MergeIntervals {
             }
         }
         return res;
+    }
+
+    public static class Interval {
+        int start;
+        int end;
+        Interval() { start = 0; end = 0; }
+        Interval(int s, int e) { start = s; end = e; }
     }
 }
