dc8dae361e57ed44d83630b3ef72dab55ca4b06b
yoyoy74662000 <yoyoy74662000@github.com>
2018-12-27 15:44:53 -0600

    add new solution 11/01

--- src/MinimumPathSum.java
+++ src/MinimumPathSum.java
@@ -25,3 +25,30 @@ public class MinimumPathSum {
         return grid[grid.length-1][grid[0].length-1];
     }
 }
+public class MinimumPathSum2 {
+    public int minPathSum(int[][] grid) {
+        return dfs(0,0,grid);
+    }
+
+    public int dfs(int i, int j, int[][] grid){
+        if(i==grid.length-1 && j==grid[0].length-1){
+            return grid[i][j];
+        }
+
+        if(i<grid.length-1 && j<grid[0].length-1){
+            int r1 = grid[i][j] + dfs(i+1, j, grid);
+            int r2 = grid[i][j] + dfs(i, j+1, grid);
+            return Math.min(r1,r2);
+        }
+
+        if(i<grid.length-1){
+            return grid[i][j] + dfs(i+1, j, grid);
+        }
+
+        if(j<grid[0].length-1){
+            return grid[i][j] + dfs(i, j+1, grid);
+        }
+
+        return 0;
+    }
+}
